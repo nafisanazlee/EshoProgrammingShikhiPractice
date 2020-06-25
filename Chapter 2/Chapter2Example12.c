fb6da94030beb92ea0daefd4fb2f1a1aa098b2fe
nafisanazlee3 <nafisanazlee3@gmail.com>
2020-06-25 23:34:48 +0600

    Updated Chapter 2 and Added Chapter 3

---
+++ Chapter 2/Chapter2Example12.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main() 
+{ 
+ char ch; 
+ printf("Enter your name.\n"); 
+ scanf("%c", &ch); 
+ printf("The first letter of your name is: %c\n", ch); 
+ return 0; 
+} 
---
+++ Chapter 2/Chapter2Example13.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main() 
+{ 
+ char ch; 
+ printf("Enter the name of your love:\n"); 
+ ch = getchar(); 
+ printf("The first letter of your love's name is: %c\n", ch); 
+ return 0; 
+}
